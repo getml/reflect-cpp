03210cdc73a510a5075fec8f74e23c3de3158b4a
Patrick Urbanke <patrick@getml.com>
2023-10-31 18:18:23 +0100

    Added more functionality to rfl::Ref

--- include/rfl/Ref.hpp
+++ include/rfl/Ref.hpp
@@ -24,8 +24,14 @@ class Ref {
 
     Ref(Ref<T>&& _other) = default;
 
-    template <class Y>
-    Ref(const Ref<Y>& _other) : ptr_(_other.ptr()) {}
+    template <class U, typename std::enable_if<std::is_convertible_v<U, T>,
+                                               bool>::type = true>
+    Ref(const Ref<U>& _other) : ptr_(_other.ptr()) {}
+
+    template <class U, typename std::enable_if<std::is_convertible_v<U, T>,
+                                               bool>::type = true>
+    Ref(Ref<U>&& _other) noexcept
+        : ptr_(std::make_shared<T>(std::forward<U>(*_other))) {}
 
     ~Ref() = default;
 
@@ -33,7 +39,13 @@ class Ref {
     T* get() const { return ptr_.get(); }
 
     /// Returns the underlying object.
+    T& operator*() { return *ptr_; }
+
+    /// Returns the underlying object.
     T& operator*() const { return *ptr_; }
+
+    /// Returns the underlying object.
+    T* operator->() { return ptr_.get(); }
 
     /// Returns the underlying object.
     T* operator->() const { return ptr_.get(); }
@@ -42,9 +54,18 @@ class Ref {
     const std::shared_ptr<T>& ptr() const { return ptr_; }
 
     /// Copy assignment operator.
-    template <class Y>
-    Ref<T>& operator=(const Ref<Y>& _other) {
+    template <class U, typename std::enable_if<std::is_convertible_v<U, T>,
+                                               bool>::type = true>
+    Ref<T>& operator=(const Ref<U>& _other) {
         ptr_ = _other.ptr();
+        return *this;
+    }
+
+    /// Move assignment operator
+    template <class U, typename std::enable_if<std::is_convertible_v<U, T>,
+                                               bool>::type = true>
+    Ref<T>& operator=(Ref<U>&& _other) noexcept {
+        ptr_ = std::make_shared<T>(std::forward<U>(*_other));
         return *this;
     }
 
@@ -67,6 +88,18 @@ class Ref {
 template <class T, class... Args>
 Ref<T> make_ref(Args&&... _args) {
     return Ref<T>::make(std::forward<Args>(_args)...);
+}
+
+template <class T1, class T2>
+inline auto operator<=>(const Ref<T1>& _t1, const Ref<T2>& _t2) {
+    return _t1.ptr() <=> _t2.ptr();
+}
+
+template <class CharT, class Traits, class T>
+inline std::basic_ostream<CharT, Traits>& operator<<(
+    std::basic_ostream<CharT, Traits>& _os, const Ref<T>& _b) {
+    _os << _b.get();
+    return _os;
 }
 
 }  // namespace rfl
