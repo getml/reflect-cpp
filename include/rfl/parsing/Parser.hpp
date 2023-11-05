6bf95a3c20a9dacc45bff00d9398051a216d0f3f
Patrick Urbanke <patrick@getml.com>
2023-11-05 12:10:18 +0100

    Minor code beautifications

--- include/rfl/parsing/Parser.hpp
+++ include/rfl/parsing/Parser.hpp
@@ -531,6 +531,26 @@ struct Parser<R, W, std::optional<T>> {
 // ----------------------------------------------------------------------------
 
 template <class R, class W, class T>
+requires AreReaderAndWriter<R, W, Ref<T>>
+struct Parser<R, W, Ref<T>> {
+  using InputVarType = typename R::InputVarType;
+  using OutputVarType = typename W::OutputVarType;
+
+  /// Expresses the variables as type T.
+  static Result<Ref<T>> read(const R& _r, const InputVarType& _var) noexcept {
+    const auto to_ref = [&](auto&& _t) { return Ref<T>::make(std::move(_t)); };
+    return Parser<R, W, std::decay_t<T>>::read(_r, _var).transform(to_ref);
+  }
+
+  /// Expresses the variable a a JSON.
+  static OutputVarType write(const W& _w, const Ref<T>& _ref) noexcept {
+    return Parser<R, W, std::decay_t<T>>::write(_w, *_ref);
+  }
+};
+
+// ----------------------------------------------------------------------------
+
+template <class R, class W, class T>
 requires AreReaderAndWriter<R, W, Result<T>>
 struct Parser<R, W, Result<T>> {
   using InputVarType = typename R::InputVarType;
@@ -570,27 +590,7 @@ struct Parser<R, W, Result<T>> {
           _w, ErrorType(make_field<"error">(_err.what())));
     };
 
-    return _r.transform(write_t).or_else(write_err).value();
-  }
-};
-
-// ----------------------------------------------------------------------------
-
-template <class R, class W, class T>
-requires AreReaderAndWriter<R, W, Ref<T>>
-struct Parser<R, W, Ref<T>> {
-  using InputVarType = typename R::InputVarType;
-  using OutputVarType = typename W::OutputVarType;
-
-  /// Expresses the variables as type T.
-  static Result<Ref<T>> read(const R& _r, const InputVarType& _var) noexcept {
-    const auto to_ref = [&](auto&& _t) { return Ref<T>::make(std::move(_t)); };
-    return Parser<R, W, std::decay_t<T>>::read(_r, _var).transform(to_ref);
-  }
-
-  /// Expresses the variable a a JSON.
-  static OutputVarType write(const W& _w, const Ref<T>& _ref) noexcept {
-    return Parser<R, W, std::decay_t<T>>::write(_w, *_ref);
+    return *_r.transform(write_t).or_else(write_err);
   }
 };
 
