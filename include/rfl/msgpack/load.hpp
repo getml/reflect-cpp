5c95c4520f8b6c6c228742a30556fec2e75807bf
Patrick Urbanke <patrick@getml.com>
2024-04-20 12:06:45 +0200

    First draft for the msgpack reader and writer; #26

---
+++ include/rfl/msgpack/load.hpp
@@ -0,0 +1,20 @@
+#ifndef RFL_MSGPACK_LOAD_HPP_
+#define RFL_MSGPACK_LOAD_HPP_
+
+#include "../Result.hpp"
+#include "../io/load_bytes.hpp"
+#include "read.hpp"
+
+namespace rfl {
+namespace msgpack {
+
+template <class T>
+Result<T> load(const std::string& _fname) {
+  const auto read_bytes = [](const auto& _bytes) { return read<T>(_bytes); };
+  return rfl::io::load_bytes(_fname).and_then(read_bytes);
+}
+
+}  // namespace msgpack
+}  // namespace rfl
+
+#endif
