83b7d7466ed9fd63cb51530f8d9bf2db1eb55c87
Patrick Urbanke <patrick@getml.com>
2024-05-12 20:59:47 +0200

    Added more experiments

--- benchmarks/json/simple_read.cpp
+++ benchmarks/json/simple_read.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <rfl/json.hpp>
 
+#include "tuple.hpp"
+
 namespace simple_read {
 
 static const std::string json_string = R"(
@@ -20,23 +22,24 @@ struct Person {
   int age;
 };
 
-static void read_using_yyjson_comparison() {
-  yyjson_doc *doc = yyjson_read(json_string.c_str(), json_string.size(), 0);
-  if (!doc) {
-    std::cout << "Could not parse document!" << std::endl;
-    return;
+struct View {
+  std::string *ptr0;
+  std::string *ptr1;
+  int *ptr2;
+};
+
+template <int _i>
+inline auto get(auto &_view) {
+  if constexpr (_i == 0) {
+    return _view.ptr0;
+  } else if constexpr (_i == 1) {
+    return _view.ptr1;
+  } else if constexpr (_i == 2) {
+    return _view.ptr2;
   }
-  yyjson_val *root = yyjson_doc_get_root(doc);
+};
 
-  const auto r = rfl::json::Reader();
-  auto homer = rfl::parsing::StructReader<
-      rfl::json::Reader, rfl::json::Writer, Person,
-      rfl::Processors<>>::read(r, rfl::json::Reader::InputVarType(root));
-
-  yyjson_doc_free(doc);
-}
-
-static void read_using_yyjson_iter() {
+static void read_using_yyjson_comparison() {
   yyjson_doc *doc = yyjson_read(json_string.c_str(), json_string.size(), 0);
   if (!doc) {
     std::cout << "Could not parse document!" << std::endl;
@@ -46,7 +49,51 @@ static void read_using_yyjson_iter() {
 
   Person homer;
 
-  const auto view = rfl::to_view(homer);
+  std::vector<rfl::Error> errors;
+  std::array<bool, 3> found;
+  std::fill(found.begin(), found.end(), false);
+  std::array<bool, 3> set;
+
+  auto view = rfl::to_view(homer);
+
+  const auto reader = rfl::json::Reader();
+
+  const auto view_reader =
+      rfl::parsing::ViewReader<rfl::json::Reader, rfl::json::Writer,
+                               decltype(view), rfl::Processors<>>(
+          &reader, &view, &found, &set, &errors);
+
+  reader.read_object(view_reader, rfl::json::Reader::InputObjectType(root));
+  if (!std::get<0>(found)) {
+    errors.push_back(rfl::Error("'first_name' not found"));
+  }
+  if (!std::get<1>(found)) {
+    errors.push_back(rfl::Error("'last_name' not found"));
+  }
+  if (!std::get<2>(found)) {
+    errors.push_back(rfl::Error("'age' not found"));
+  }
+  if (errors.size() != 0) {
+    std::cout << "Some errors occurred:" << std::endl;
+    for (const auto &err : errors) {
+      std::cout << err.what() << std::endl;
+    }
+  }
+
+  yyjson_doc_free(doc);
+}
+
+static void read_using_yyjson_comparison2() {
+  yyjson_doc *doc = yyjson_read(json_string.c_str(), json_string.size(), 0);
+  if (!doc) {
+    std::cout << "Could not parse document!" << std::endl;
+    return;
+  }
+  yyjson_val *root = yyjson_doc_get_root(doc);
+
+  Person homer;
+
+  auto view = View{&homer.first_name, &homer.last_name, &homer.age};
 
   std::vector<std::string> errors;
   std::array<bool, 3> found;
@@ -64,7 +111,7 @@ static void read_using_yyjson_iter() {
         errors.push_back(
             "Error reading 'first_name': Could not cast to string.");
       }
-      *rfl::get<"first_name">(view) = first_name;
+      *get<0>(view) = first_name;
       std::get<0>(found) = true;
     } else if (name == "last_name") {
       auto last_name = yyjson_get_str(v);
@@ -72,13 +119,79 @@ static void read_using_yyjson_iter() {
         errors.push_back(
             "Error reading 'last_name': Could not cast to string.");
       }
-      *rfl::get<"last_name">(view) = last_name;
+      *get<1>(view) = last_name;
       std::get<1>(found) = true;
     } else if (name == "age") {
       if (!yyjson_is_int(v)) {
         errors.push_back("Error reading 'age': Could not cast to int.");
       }
-      *rfl::get<"age">(view) = yyjson_get_int(v);
+      *get<2>(view) = yyjson_get_int(v);
+      std::get<2>(found) = true;
+    }
+  }
+  if (!std::get<0>(found)) {
+    errors.push_back("'first_name' not found");
+  }
+  if (!std::get<1>(found)) {
+    errors.push_back("'last_name' not found");
+  }
+  if (!std::get<2>(found)) {
+    errors.push_back("'age' not found");
+  }
+  if (errors.size() != 0) {
+    std::cout << "Some errors occurred:" << std::endl;
+    for (const auto &err : errors) {
+      std::cout << err << std::endl;
+    }
+  }
+
+  // std::get<0>(view)->~string();
+  // std::get<1>(view)->~string();
+
+  yyjson_doc_free(doc);
+}
+
+static void read_using_yyjson_iter() {
+  yyjson_doc *doc = yyjson_read(json_string.c_str(), json_string.size(), 0);
+  if (!doc) {
+    std::cout << "Could not parse document!" << std::endl;
+    return;
+  }
+  yyjson_val *root = yyjson_doc_get_root(doc);
+
+  Person homer;
+
+  std::vector<std::string> errors;
+  std::array<bool, 3> found;
+  std::fill(found.begin(), found.end(), false);
+
+  yyjson_obj_iter iter;
+  yyjson_obj_iter_init(root, &iter);
+  yyjson_val *key;
+  while ((key = yyjson_obj_iter_next(&iter))) {
+    const auto name = std::string_view(yyjson_get_str(key));
+    const auto v = yyjson_obj_iter_get_val(key);
+    if (name == "first_name") {
+      auto first_name = yyjson_get_str(v);
+      if (first_name == NULL) {
+        errors.push_back(
+            "Error reading 'first_name': Could not cast to string.");
+      }
+      homer.first_name = first_name;
+      std::get<0>(found) = true;
+    } else if (name == "last_name") {
+      auto last_name = yyjson_get_str(v);
+      if (last_name == NULL) {
+        errors.push_back(
+            "Error reading 'last_name': Could not cast to string.");
+      }
+      homer.last_name = last_name;
+      std::get<1>(found) = true;
+    } else if (name == "age") {
+      if (!yyjson_is_int(v)) {
+        errors.push_back("Error reading 'age': Could not cast to int.");
+      }
+      homer.age = yyjson_get_int(v);
       std::get<2>(found) = true;
     }
   }
@@ -115,6 +228,11 @@ static void BM_simple_read_yyjson_comparison(benchmark::State &state) {
   for (auto _ : state) read_using_yyjson_comparison();
 }
 BENCHMARK(BM_simple_read_yyjson_comparison);
+
+static void BM_simple_read_yyjson_comparison2(benchmark::State &state) {
+  for (auto _ : state) read_using_yyjson_comparison2();
+}
+BENCHMARK(BM_simple_read_yyjson_comparison2);
 
 }  // namespace simple_read
 
