0e8cceff5d38b0a404589f8c589b3cf6d1bb8a90
Patrick Urbanke <patrick@getml.com>
2024-05-25 21:52:13 +0200

    Got rid of recursive assignment

--- benchmarks/json/simple_read.cpp
+++ benchmarks/json/simple_read.cpp
@@ -3,8 +3,7 @@
 #include <array>
 #include <iostream>
 #include <rfl/json.hpp>
-
-#include "tuple.hpp"
+#include <type_traits>
 
 namespace simple_read {
 
@@ -12,20 +11,20 @@ static const std::string json_string = R"(
     {
         "first_name": "Homer",
         "last_name": "Simpson",
-        "age": 45
+        "age": "45"
     }
 )";
 
 struct Person {
   std::string first_name;
   std::string last_name;
-  int age;
+  std::string age;
 };
 
 struct View {
   std::string *ptr0;
   std::string *ptr1;
-  int *ptr2;
+  std::string *ptr2;
 };
 
 template <int _i>
@@ -49,38 +48,51 @@ static void read_using_yyjson_comparison() {
 
   Person homer;
 
-  std::vector<rfl::Error> errors;
-  std::array<bool, 3> found;
-  std::fill(found.begin(), found.end(), false);
-  std::array<bool, 3> set;
-
   auto view = rfl::to_view(homer);
 
   const auto reader = rfl::json::Reader();
 
-  const auto view_reader =
-      rfl::parsing::ViewReader<rfl::json::Reader, rfl::json::Writer,
-                               decltype(view), rfl::Processors<>>(
-          &reader, &view, &found, &set, &errors);
+  const auto err = rfl::parsing::Parser<
+      rfl::json::Reader, rfl::json::Writer, decltype(view),
+      rfl::Processors<>>::read_view(reader,
+                                    rfl::json::Reader::InputVarType(root),
+                                    &view);
 
-  reader.read_object(view_reader, rfl::json::Reader::InputObjectType(root));
-  if (!std::get<0>(found)) {
-    errors.push_back(rfl::Error("'first_name' not found"));
-  }
-  if (!std::get<1>(found)) {
-    errors.push_back(rfl::Error("'last_name' not found"));
-  }
-  if (!std::get<2>(found)) {
-    errors.push_back(rfl::Error("'age' not found"));
-  }
-  if (errors.size() != 0) {
-    std::cout << "Some errors occurred:" << std::endl;
-    for (const auto &err : errors) {
-      std::cout << err.what() << std::endl;
-    }
+  if (err) {
+    std::cout << err->what() << std::endl;
   }
 
   yyjson_doc_free(doc);
+}
+
+template <class ViewType, int i>
+void assign_one(const rfl::json::Reader &_r,
+                const std::string_view &_current_name,
+                const rfl::json::Reader::InputVarType &_var, ViewType *_view,
+                auto *_errors, auto *_found, bool *_ok) {
+  using FieldType = std::tuple_element_t<i, typename ViewType::Fields>;
+  using T = std::remove_pointer_t<typename FieldType::Type>;
+  constexpr auto name = FieldType::name();
+  if (!(*_ok) && _current_name == name) {
+    auto r = rfl::parsing::Parser<rfl::json::Reader, rfl::json::Writer, T,
+                                  rfl::Processors<>>::read(_r, _var);
+    if (!r) {
+      _errors->push_back(r.error()->what());
+    }
+    *rfl::get<i>(*_view) = std::move(*r);
+    std::get<i>(*_found) = true;
+  }
+}
+
+template <class ViewType, int... is>
+void assign_all(const rfl::json::Reader &_r,
+                const std::string_view &_current_name,
+                const rfl::json::Reader::InputVarType &_var, ViewType *_view,
+                auto *_errors, auto *_found, bool *_ok,
+                std::integer_sequence<int, is...>) {
+  (assign_one<ViewType, is>(_r, _current_name, _var, _view, _errors, _found,
+                            _ok),
+   ...);
 }
 
 static void read_using_yyjson_comparison2() {
@@ -93,7 +105,9 @@ static void read_using_yyjson_comparison2() {
 
   Person homer;
 
-  auto view = View{&homer.first_name, &homer.last_name, &homer.age};
+  auto view = rfl::to_view(homer);
+
+  const auto reader = rfl::json::Reader();
 
   std::vector<std::string> errors;
   std::array<bool, 3> found;
@@ -105,29 +119,10 @@ static void read_using_yyjson_comparison2() {
   while ((key = yyjson_obj_iter_next(&iter))) {
     const auto name = std::string_view(yyjson_get_str(key));
     const auto v = yyjson_obj_iter_get_val(key);
-    if (name == "first_name") {
-      auto first_name = yyjson_get_str(v);
-      if (first_name == NULL) {
-        errors.push_back(
-            "Error reading 'first_name': Could not cast to string.");
-      }
-      *get<0>(view) = first_name;
-      std::get<0>(found) = true;
-    } else if (name == "last_name") {
-      auto last_name = yyjson_get_str(v);
-      if (last_name == NULL) {
-        errors.push_back(
-            "Error reading 'last_name': Could not cast to string.");
-      }
-      *get<1>(view) = last_name;
-      std::get<1>(found) = true;
-    } else if (name == "age") {
-      if (!yyjson_is_int(v)) {
-        errors.push_back("Error reading 'age': Could not cast to int.");
-      }
-      *get<2>(view) = yyjson_get_int(v);
-      std::get<2>(found) = true;
-    }
+    bool ok = false;
+    assign_all<typeof(view)>(reader, name, rfl::json::Reader::InputVarType(v),
+                             &view, &errors, &found, &ok,
+                             std::make_integer_sequence<int, view.size()>());
   }
   if (!std::get<0>(found)) {
     errors.push_back("'first_name' not found");
@@ -144,9 +139,6 @@ static void read_using_yyjson_comparison2() {
       std::cout << err << std::endl;
     }
   }
-
-  // std::get<0>(view)->~string();
-  // std::get<1>(view)->~string();
 
   yyjson_doc_free(doc);
 }
@@ -188,10 +180,11 @@ static void read_using_yyjson_iter() {
       homer.last_name = last_name;
       std::get<1>(found) = true;
     } else if (name == "age") {
-      if (!yyjson_is_int(v)) {
-        errors.push_back("Error reading 'age': Could not cast to int.");
+      auto age = yyjson_get_str(v);
+      if (age == NULL) {
+        errors.push_back("Error reading 'age': Could not cast to string.");
       }
-      homer.age = yyjson_get_int(v);
+      homer.age = age;
       std::get<2>(found) = true;
     }
   }
@@ -219,11 +212,6 @@ static void BM_simple_read_yyjson_iter(benchmark::State &state) {
 }
 BENCHMARK(BM_simple_read_yyjson_iter);
 
-static void BM_simple_read_reflect_cpp(benchmark::State &state) {
-  for (auto _ : state) rfl::json::read<Person>(json_string);
-}
-BENCHMARK(BM_simple_read_reflect_cpp);
-
 static void BM_simple_read_yyjson_comparison(benchmark::State &state) {
   for (auto _ : state) read_using_yyjson_comparison();
 }
@@ -233,6 +221,11 @@ static void BM_simple_read_yyjson_comparison2(benchmark::State &state) {
   for (auto _ : state) read_using_yyjson_comparison2();
 }
 BENCHMARK(BM_simple_read_yyjson_comparison2);
+
+static void BM_simple_read_reflect_cpp(benchmark::State &state) {
+  for (auto _ : state) rfl::json::read<Person>(json_string);
+}
+BENCHMARK(BM_simple_read_reflect_cpp);
 
 }  // namespace simple_read
 
--- include/rfl/parsing/ViewReader.hpp
+++ include/rfl/parsing/ViewReader.hpp
@@ -24,33 +24,9 @@ class ViewReader {
 
   ~ViewReader() = default;
 
-  template <size_t _i = 0>
   void read(const std::string_view& _name, const InputVarType& _var) const {
-    if constexpr (_i < size_) {
-      using FieldType = std::tuple_element_t<_i, typename ViewType::Fields>;
-      using OriginalType = std::remove_cvref_t<typename FieldType::Type>;
-      using CurrentType =
-          std::remove_cvref_t<std::remove_pointer_t<OriginalType>>;
-      constexpr auto current_name = FieldType::name();
-      if (!std::get<_i>(*found_) && _name == current_name) {
-        auto res = Parser<R, W, CurrentType, ProcessorsType>::read(*r_, _var);
-        if (res) {
-          if constexpr (std::is_pointer_v<OriginalType>) {
-            move_to(rfl::get<_i>(*view_), &(*res));
-          } else {
-            rfl::get<_i>(*view_) = *res;
-          }
-          std::get<_i>(*set_) = true;
-        } else {
-          errors_->push_back(Error("Failed to parse field '" +
-                                   std::string(current_name) +
-                                   "': " + res.error()->what()));
-        }
-        std::get<_i>(*found_) = true;
-        return;
-      }
-      read<_i + 1>(_name, _var);
-    }
+    assign_all(*r_, _name, _var, view_, errors_, found_, set_,
+               std::make_integer_sequence<int, size_>());
   }
 
   /// Because of the way we have allocated the fields, we need to manually
@@ -79,6 +55,36 @@ class ViewReader {
   }
 
  private:
+  template <int i>
+  static void assign_one(const R& _r, const std::string_view& _current_name,
+                         const auto& _var, ViewType* _view, auto* _errors,
+                         auto* _found, auto* _set, bool* _ok) {
+    using FieldType = std::tuple_element_t<i, typename ViewType::Fields>;
+    using T = std::remove_pointer_t<typename FieldType::Type>;
+    constexpr auto name = FieldType::name();
+    if (!(*_ok) && _current_name == name) {
+      auto r = rfl::parsing::Parser<R, W, T, rfl::Processors<>>::read(_r, _var);
+      if (!r) {
+        _errors->push_back(std::move(*r.error()));
+      } else {
+        std::get<i>(*_set) = true;
+      }
+      move_to(rfl::get<i>(*_view), &(*r));
+      std::get<i>(*_found) = true;
+      *_ok = true;
+    }
+  }
+
+  template <int... is>
+  static void assign_all(const R& _r, const std::string_view& _current_name,
+                         const auto& _var, ViewType* _view, auto* _errors,
+                         auto* _found, auto* _set,
+                         std::integer_sequence<int, is...> _seq) {
+    bool ok = false;
+    (assign_one<is>(_r, _current_name, _var, _view, _errors, _found, _set, &ok),
+     ...);
+  }
+
   template <class T>
   void call_destructor_on_array(const size_t _size, T* _ptr) const {
     for (size_t i = 0; i < _size; ++i) {
@@ -91,7 +97,7 @@ class ViewReader {
   }
 
   template <class Target, class Source>
-  void move_to(Target* _t, Source* _s) const {
+  static void move_to(Target* _t, Source* _s) {
     if constexpr (std::is_const_v<Target>) {
       return move_to(const_cast<std::remove_const_t<Target>*>(_t), _s);
     } else if constexpr (!internal::is_array_v<Source> &&
