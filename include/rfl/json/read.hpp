18e122379d218ec972250036def2c0ea7b44d710
Patrick Urbanke <patrick@getml.com>
2024-05-28 22:35:12 +0200

    Removed brute optimization

--- include/rfl/json/read.hpp
+++ include/rfl/json/read.hpp
@@ -33,24 +33,9 @@ Result<internal::wrap_in_rfl_array_t<T>> read(const std::string& _json_str) {
   }
   yyjson_val* root = yyjson_doc_get_root(doc);
   const auto r = Reader();
-  if constexpr (std::is_class_v<T> && std::is_aggregate_v<T>) {
-    using ProcessorsType = Processors<Ps...>;
-    alignas(T) unsigned char buf[sizeof(T)];
-    auto ptr = reinterpret_cast<T*>(buf);
-    auto view = ProcessorsType::template process<T>(to_view(*ptr));
-    using ViewType = std::remove_cvref_t<decltype(view)>;
-    const auto err = Parser<ViewType, ProcessorsType>::read_view(
-        r, InputVarType(root), &view);
-    yyjson_doc_free(doc);
-    if (err) [[unlikely]] {
-      return *err;
-    }
-    return std::move(*ptr);
-  } else {
-    auto res = Parser<T, Processors<Ps...>>::read(r, InputVarType(root));
-    yyjson_doc_free(doc);
-    return res;
-  }
+  auto res = Parser<T, Processors<Ps...>>::read(r, InputVarType(root));
+  yyjson_doc_free(doc);
+  return res;
 }
 
 /// Parses an object from a stringstream.
