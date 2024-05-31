4c4141b4ae1e9e83f267299b33127b912a28925d
Patrick Urbanke <patrick@getml.com>
2024-05-31 23:38:13 +0200

    Added test for templates; #108

---
+++ tests/json/test_template.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <rfl.hpp>
+#include <rfl/json.hpp>
+#include <source_location>
+#include <string>
+#include <vector>
+
+#include "write_and_read.hpp"
+
+namespace test_template {
+
+template <typename T>
+struct A {
+  int code;
+  std::string msg;
+  T data;
+};
+
+struct B {
+  int some_val;
+};
+
+TEST(json, test_template) {
+  const auto a = A<B>{.code = 0, .msg = "hello", .data = B{.some_val = 1}};
+
+  write_and_read(a, R"({"code":0,"msg":"hello","data":{"some_val":1}})");
+}
+
+}  // namespace test_template
