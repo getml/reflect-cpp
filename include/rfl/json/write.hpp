0342e5c9a224039146a9f4261ac3cb695457422a
Dr. Patrick Urbanke (劉自成) <patrick_urbanke@web.de>
2024-06-13 00:37:35 +0200

    Separate third party libraries; resolves #79 (#119)

--- include/rfl/json/write.hpp
+++ include/rfl/json/write.hpp
@@ -1,7 +1,11 @@
 #ifndef RFL_JSON_WRITE_HPP_
 #define RFL_JSON_WRITE_HPP_
 
+#ifdef REFLECTCPP_NO_BUNDLED_DEPENDENCIES
 #include <yyjson.h>
+#else
+#include "../thirdparty/yyjson.h"
+#endif
 
 #include <ostream>
 #include <sstream>
