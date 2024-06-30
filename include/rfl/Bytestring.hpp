986ddb594c277be28f1441cec26594a558cc1b62
Patrick Urbanke <patrick@getml.com>
2024-06-30 16:43:43 +0200

    Added rfl::Bytestring and wrote msgpack support

---
+++ include/rfl/Bytestring.hpp
@@ -0,0 +1,13 @@
+#ifndef RFL_BYTESTRING_HPP_
+#define RFL_BYTESTRING_HPP_
+
+#include <cstddef>
+#include <string>
+
+namespace rfl {
+
+using Bytestring = std::basic_string<std::byte>;
+
+}  // namespace rfl
+
+#endif
