617997a175673387f779b97c4fb08f161bc6be8f
Daedie <Daedie@gmail.com>
2024-07-06 15:12:24 +0200

    fixed gcc build error

--- tests/json/test_reflector.cpp
+++ tests/json/test_reflector.cpp
@@ -22,11 +22,11 @@ struct Reflector<Person> {
     std::string last_name;
     std::vector<Person> children;
   };
-  static constexpr Person to(const ReflType& v) noexcept {
+  static Person to(const ReflType& v) noexcept {
     return {v.first_name, v.last_name, v.children};
   }
 
-  static constexpr ReflType from(const Person& v) {
+  static ReflType from(const Person& v) {
     return {v.first_name, v.last_name, v.children};
   }
 };
