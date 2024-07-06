5c0961647918bc2616fb1e95b108a7cdb87e7a49
Patrick Urbanke <patrick@getml.com>
2024-07-06 09:49:59 +0200

    Added an element_index function

---
+++ include/rfl/internal/element_index.hpp
@@ -0,0 +1,24 @@
+#ifndef RFL_INTERNAL_ELEMENT_INDEX_HPP_
+#define RFL_INTERNAL_ELEMENT_INDEX_HPP_
+
+#include <type_traits>
+
+namespace rfl::internal {
+
+template <int _i, class T, class Head, Tail...>
+consteval int find_element_index() {
+  if constexpr (std::is_same_v<T, Head>()) {
+    return _i;
+  } else {
+    return find_element_index<_i + 1, T, Tail...>();
+  }
+}
+
+template <class T, class AlternativeTypes...>
+consteval int element_index() {
+  return find_element_index<0, T, AlternativeTypes...>();
+}
+
+}  // namespace rfl::internal
+
+#endif
