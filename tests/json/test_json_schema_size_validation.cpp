0f7806f9e19993a20d39fc19eee638d6f648bc6e
toropov <toropov@protei.ru>
2024-07-10 20:18:26 +0300

    rewrite test for JSON schema size validation

--- tests/json/test_json_schema_size_validation.cpp
+++ tests/json/test_json_schema_size_validation.cpp
@@ -1,45 +1,81 @@
-#include <iostream>
+#include <array>
 #include <rfl.hpp>
 #include <rfl/json.hpp>
+#include <set>
 #include <string>
-#include <tuple>
-#include <variant>
 #include <vector>
 
-#include "write_and_read.hpp"
+#include "gtest/gtest.h"
 
 namespace test_json_schema_size_validation {
 
-struct TestStruct {
-  template <typename T, typename... Validators>
-  using SizedAnyOf = rfl::Validator<T, rfl::Size<rfl::AnyOf<Validators...>>>;
-  template <typename T, typename... Validators>
-  using SizedAllOf = rfl::Validator<T, rfl::Size<rfl::AllOf<Validators...>>>;
-  template <typename T, auto Min, auto Max>
-  using SizedMinMax = rfl::Validator<T, rfl::Size<rfl::Minimum<Min>>,
-                                     rfl::Size<rfl::Maximum<Max>>>;
+using rfl::EqualTo;
+using rfl::Maximum;
+using rfl::Minimum;
+using rfl::json::to_schema;
 
-  SizedMinMax<std::string, 1, 2> a;
-  SizedMinMax<std::vector<int>, 2, 3> b;
-  SizedMinMax<std::set<int>, 3, 4> c;
+template <typename T, typename... Validators>
+using SizedAnyOf = rfl::Validator<T, rfl::Size<rfl::AnyOf<Validators...>>>;
+template <typename T, typename... Validators>
+using SizedAllOf = rfl::Validator<T, rfl::Size<rfl::AllOf<Validators...>>>;
+template <typename T, auto Min, auto Max>
+using SizedMinMax =
+    rfl::Validator<T, rfl::Size<Minimum<Min>>, rfl::Size<Maximum<Max>>>;
 
-  SizedAnyOf<std::string, rfl::EqualTo<1>, rfl::EqualTo<3>> d;
-  SizedAnyOf<std::vector<int>, rfl::EqualTo<2>, rfl::EqualTo<4>> e;
-  SizedAnyOf<std::set<int>, rfl::EqualTo<3>, rfl::EqualTo<5>> f;
+TEST(json, test_json_schema_sized_vector_min1_max2) {
+  constexpr auto expected =
+      R"({"$schema":"https://json-schema.org/draft/2020-12/schema","allOf":[{"type":"array","items":{"type":"integer"},"minItems":1},{"type":"array","items":{"type":"integer"},"maxItems":2}],"definitions":{}})";
 
-  // AllOf<Size<Minimum<X>>, Size<Maximum<Y>>> expected to be the same as
-  // Size<AllOf<Minimum<X>, Maximum<Y>>>, so a, b, and c should produce the same
-  // schema as g, h, and i
-  SizedAllOf<std::string, rfl::Minimum<1>, rfl::Maximum<2>> g;
-  SizedAllOf<std::vector<int>, rfl::Minimum<2>, rfl::Maximum<3>> h;
-  SizedAllOf<std::set<int>, rfl::Minimum<3>, rfl::Maximum<4>> i;
+  auto actual = to_schema<SizedMinMax<std::vector<int>, 1, 2>>();
+  ASSERT_EQ(expected, actual);
+  actual = to_schema<SizedAllOf<std::vector<int>, Minimum<1>, Maximum<2>>>();
+  ASSERT_EQ(expected, actual);
+}
 
-  std::array<int, 3> j;
-};
+TEST(json, test_json_schema_sized_set_min2_max3) {
+  constexpr auto expected =
+      R"({"$schema":"https://json-schema.org/draft/2020-12/schema","allOf":[{"type":"array","items":{"type":"integer"},"minItems":2},{"type":"array","items":{"type":"integer"},"maxItems":3}],"definitions":{}})";
 
-TEST(json, test_json_schema_size_validation) {
-  constexpr auto expected_schema =
-      R"({"$schema":"https://json-schema.org/draft/2020-12/schema","$ref":"#/definitions/test_json_schema_size_validation__TestStruct","definitions":{"test_json_schema_size_validation__TestStruct":{"type":"object","properties":{"a":{"allOf":[{"type":"string","minLength":1},{"type":"string","maxLength":2}]},"b":{"allOf":[{"type":"array","items":{"type":"integer"},"minItems":2},{"type":"array","items":{"type":"integer"},"maxItems":3}]},"c":{"allOf":[{"type":"array","items":{"type":"integer"},"minItems":3},{"type":"array","items":{"type":"integer"},"maxItems":4}]},"d":{"anyOf":[{"type":"string","minLength":1,"maxLength":1},{"type":"string","minLength":3,"maxLength":3}]},"e":{"anyOf":[{"type":"array","items":{"type":"integer"},"minItems":2,"maxItems":2},{"type":"array","items":{"type":"integer"},"minItems":4,"maxItems":4}]},"f":{"anyOf":[{"type":"array","items":{"type":"integer"},"minItems":3,"maxItems":3},{"type":"array","items":{"type":"integer"},"minItems":5,"maxItems":5}]},"g":{"allOf":[{"type":"string","minLength":1},{"type":"string","maxLength":2}]},"h":{"allOf":[{"type":"array","items":{"type":"integer"},"minItems":2},{"type":"array","items":{"type":"integer"},"maxItems":3}]},"i":{"allOf":[{"type":"array","items":{"type":"integer"},"minItems":3},{"type":"array","items":{"type":"integer"},"maxItems":4}]},"j":{"type":"array","items":{"type":"integer"},"minItems":3,"maxItems":3}},"required":["a","b","c","d","e","f","g","h","i","j"]}}})";
-  ASSERT_EQ(expected_schema, rfl::json::to_schema<TestStruct>());
+  auto actual = to_schema<SizedMinMax<std::set<int>, 2, 3>>();
+  ASSERT_EQ(expected, actual);
+  actual = to_schema<SizedAllOf<std::set<int>, Minimum<2>, Maximum<3>>>();
+  ASSERT_EQ(expected, actual);
+}
+
+TEST(json, test_json_schema_sized_string_min4_max6) {
+  constexpr auto expected =
+      R"({"$schema":"https://json-schema.org/draft/2020-12/schema","allOf":[{"type":"string","minLength":4},{"type":"string","maxLength":6}],"definitions":{}})";
+
+  auto actual = to_schema<SizedMinMax<std::string, 4, 6>>();
+  ASSERT_EQ(expected, actual);
+  actual = to_schema<SizedAllOf<std::string, Minimum<4>, Maximum<6>>>();
+  ASSERT_EQ(expected, actual);
+}
+
+TEST(json, test_json_schema_sized_vector_anyof_eq3_eq7) {
+  constexpr auto expected =
+      R"({"$schema":"https://json-schema.org/draft/2020-12/schema","anyOf":[{"type":"array","items":{"type":"integer"},"minItems":3,"maxItems":3},{"type":"array","items":{"type":"integer"},"minItems":7,"maxItems":7}],"definitions":{}})";
+
+  auto actual =
+      to_schema<SizedAnyOf<std::vector<int>, EqualTo<3>, EqualTo<7>>>();
+  ASSERT_EQ(expected, actual);
+}
+
+TEST(json, test_json_schema_sized_set_anyof_eq15_eq16) {
+  constexpr auto expected =
+      R"({"$schema":"https://json-schema.org/draft/2020-12/schema","anyOf":[{"type":"array","items":{"type":"integer"},"minItems":16,"maxItems":16},{"type":"array","items":{"type":"integer"},"minItems":16,"maxItems":16}],"definitions":{}})";
+
+  auto actual =
+      to_schema<SizedAnyOf<std::vector<int>, EqualTo<16>, EqualTo<16>>>();
+  ASSERT_EQ(expected, actual);
+}
+
+TEST(json, test_json_schema_sized_string_anyof_eq1_eq10) {
+  constexpr auto expected =
+      R"({"$schema":"https://json-schema.org/draft/2020-12/schema","anyOf":[{"type":"array","items":{"type":"integer"},"minItems":1,"maxItems":1},{"type":"array","items":{"type":"integer"},"minItems":10,"maxItems":10}],"definitions":{}})";
+
+  auto actual =
+      to_schema<SizedAnyOf<std::vector<int>, EqualTo<1>, EqualTo<10>>>();
+  ASSERT_EQ(expected, actual);
 }
 }  // namespace test_json_schema_size_validation
