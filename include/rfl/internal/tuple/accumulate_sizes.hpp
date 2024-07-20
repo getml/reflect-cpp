3bec99366f20a93f623bf32f7f49ce33a293ab49
Patrick Urbanke <patrick@getml.com>
2024-07-20 18:20:04 +0200

    Fixed alignment issues

--- include/rfl/internal/tuple/accumulate_sizes.hpp
+++ include/rfl/internal/tuple/accumulate_sizes.hpp
@@ -12,20 +12,26 @@ struct SizeWrapper {};
 template <unsigned long _last, unsigned long... _is>
 struct Sizes {
   static consteval auto to_array() {
-    return std::array<unsigned long, sizeof...(_is)>({_is...});
+    return std::array<unsigned long, sizeof...(_is) + 1>({_is..., _last});
   }
 };
 
 template <class T, unsigned long _last, unsigned long... _is>
 consteval auto operator+(const Sizes<_last, _is...>& _sizes,
                          const SizeWrapper<T>& _w) {
-  constexpr auto last = _last + alignof(T);
-  return Sizes<last, _is..., last>{};
+  if constexpr (_last % alignof(T) == 0) {
+    constexpr auto last_new = _last + sizeof(T);
+    return Sizes<last_new, _is..., _last>{};
+  } else {
+    constexpr auto last_corrected = _last + alignof(T) - (_last % alignof(T));
+    constexpr auto last_new = last_corrected + sizeof(T);
+    return Sizes<last_new, _is..., last_corrected>{};
+  }
 }
 
 template <class... Types>
 consteval auto accumulate_sizes() {
-  return (Sizes<0, 0>{} + ... + SizeWrapper<Types>{}).to_array();
+  return (Sizes<0>{} + ... + SizeWrapper<Types>{}).to_array();
 }
 
 }  // namespace rfl::internal::tuple
