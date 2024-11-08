716de7b6fdce722f0a09d338349d968a39d5ae9e
Manuel Bellersen <Urfoex@users.noreply.github.com>
2024-11-08 13:39:08 +0100

    Emplace into optional to be able to visit variant containers containi… (#234)

--- tests/json/test_rfl_variant_visit_move_only.cpp
+++ tests/json/test_rfl_variant_visit_move_only.cpp
@@ -1,5 +1,4 @@
 #include <cassert>
-#include <iostream>
 #include <memory>
 #include <rfl.hpp>
 #include <rfl/json.hpp>
@@ -19,6 +18,10 @@ struct Square {
   std::unique_ptr<double> width;
 };
 
+struct Circle {
+  double const radius;
+};
+
 using Shapes = rfl::Variant<Rectangle, Square>;
 
 TEST(json, test_rfl_variant_visit_move_only) {
@@ -28,5 +31,25 @@ TEST(json, test_rfl_variant_visit_move_only) {
     return _s.width;
   };
   EXPECT_EQ(*rfl::visit(get_width, r), 5.0);
+}
+
+TEST(json, test_rfl_variant_visit_return_move_only_const) {
+  auto const circle = Circle{.radius = 10};
+  auto const variant = rfl::Variant<Circle>{circle};
+  const auto get_radius = [](const auto& object) -> const double& {
+    return object.radius;
+  };
+  auto const result = rfl::visit(get_radius, variant);
+  EXPECT_EQ(result, circle.radius);
+}
+
+TEST(json, test_rfl_variant_visit_return_move_only) {
+  auto circle = Circle{.radius = 10};
+  auto variant = rfl::Variant<Circle>{circle};
+  auto const get_radius = [](auto& object) -> double const& {
+    return object.radius;
+  };
+  auto result = rfl::visit(get_radius, variant);
+  EXPECT_EQ(result, circle.radius);
 }
 }  // namespace test_rfl_variant_visit_move_only
