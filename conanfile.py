e164dc5c596fff674dfefc84ecc7ada99a6ba5ae
Dr. Patrick Urbanke <patrick@getml.com>
2024-11-24 13:43:05 +0100

    Removed hand-written validation

--- conanfile.py
+++ conanfile.py
@@ -89,17 +89,6 @@ class ReflectCppConan(ConanFile):
     def validate(self):
         if self.settings.get_safe("compiler.cppstd"):
             check_min_cppstd(self, self._min_cppstd)
-        minimum_version = self._compilers_minimum_version.get(
-            str(self.settings.compiler), False
-        )
-        if (
-            minimum_version
-            and Version(self.settings.compiler.version) < minimum_version
-        ):
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support. "
-                f"Your compiler only supports {Version(self.settings.compiler.version)}, but we need {minimum_version}."
-            )
 
     def layout(self):
         cmake_layout(self, src_folder=".")
