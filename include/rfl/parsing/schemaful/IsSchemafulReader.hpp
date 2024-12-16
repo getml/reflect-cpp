809196298e28066c89aa1d65f2b532758c8ae663
Dr. Patrick Urbanke <patrick@getml.com>
2024-12-16 22:02:22 +0100

    Added comments for IsSchemafulReader and IsSchemafulWriter

--- include/rfl/parsing/schemaful/IsSchemafulReader.hpp
+++ include/rfl/parsing/schemaful/IsSchemafulReader.hpp
@@ -32,18 +32,27 @@ concept IsSchemafulReader =
     requires(R r, typename R::InputVarType var, typename R::InputObjectType obj,
              typename R::InputMapType m, typename R::InputUnionType u,
              MockMapReader<R> map_reader, MockObjectReader<R> object_reader) {
+      /// A schemaful reader needs to differentiate between objects, for which
+      /// the field names are known at compile time and maps, for which the
+      /// field names are not known at compile time.
       { r.read_map(map_reader, m) } -> std::same_as<std::optional<Error>>;
 
+      /// A schemaful reader can read fields by order and does not have to
+      /// compare strings - the correct order of the fields is guaranteed by the
+      /// schema.
       {
         r.read_object(object_reader, obj)
       } -> std::same_as<std::optional<Error>>;
 
+      /// A schemaful reader needs an explicit union type.
       {
         r.template read_union<MockVariantType, MockUnionReader<R>>(u)
       } -> std::same_as<rfl::Result<MockVariantType>>;
 
+      /// It needs to be possible to transform variables to maps.
       { r.to_map(var) } -> std::same_as<rfl::Result<typename R::InputMapType>>;
 
+      /// It needs to be possible to transform variables to unions.
       {
         r.to_union(var)
       } -> std::same_as<rfl::Result<typename R::InputUnionType>>;
