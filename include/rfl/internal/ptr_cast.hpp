72cda49089df2e95a37fd3aa39bb45b8aa7d73bb
Dr. Patrick Urbanke <patrick@getml.com>
2024-12-17 23:35:17 +0100

    Try going back to bit_cast

--- include/rfl/internal/ptr_cast.hpp
+++ include/rfl/internal/ptr_cast.hpp
@@ -1,6 +1,8 @@
 #ifndef RFL_INTERNAL_PTRCAST_HPP_
 #define RFL_INTERNAL_PTRCAST_HPP_
 
+#include <bit>
+
 namespace rfl::internal {
 
 /// Normally, we would use std::launder(reinterpret_cast<...>(...)),
@@ -9,13 +11,14 @@ namespace rfl::internal {
 
 template <class T1, class T2>
 inline T1 ptr_cast(T2* _ptr) {
-  return static_cast<T1>(static_cast<void*>(_ptr));
+  return std::bit_cast<T1>(_ptr);
+  // return static_cast<T1>(static_cast<void*>(_ptr));
 }
-
+/*
 template <class T1, class T2>
 inline T1 ptr_cast(const T2* _ptr) {
-  return static_cast<T1>(static_cast<const void*>(_ptr));
-}
+  //return static_cast<T1>(static_cast<const void*>(_ptr));
+}*/
 
 }  // namespace rfl::internal
 
