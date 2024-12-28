886017c031ca2b0cc8ac53e0cff0982c8c42d165
Dr. Patrick Urbanke <patrick@getml.com>
2024-12-28 12:14:31 +0100

    Use a memoization-like pattern to generate the schema

--- include/rfl/capnproto/to_schema.hpp
+++ include/rfl/capnproto/to_schema.hpp
@@ -7,13 +7,13 @@
 
 #include "../Literal.hpp"
 #include "../Processors.hpp"
+#include "../Result.hpp"
 #include "../SnakeCaseToCamelCase.hpp"
 #include "../Variant.hpp"
 #include "../json.hpp"
 #include "../parsing/schema/Type.hpp"
 #include "../parsing/schema/ValidationType.hpp"
 #include "../parsing/schema/make.hpp"
-#include "Reader.hpp"
 #include "Schema.hpp"
 #include "Writer.hpp"
 #include "schema/Type.hpp"
@@ -23,14 +23,26 @@ namespace rfl::capnproto {
 std::string to_string_representation(
     const parsing::schema::Definition& internal_schema);
 
+template <class T, class... Ps>
+struct SchemaHolder {
+  rfl::Result<Schema<T>> schema_;
+  static SchemaHolder<T, Ps...> make() noexcept {
+    const auto internal_schema =
+        parsing::schema::make<Reader, Writer, T,
+                              Processors<SnakeCaseToCamelCase, Ps...>>();
+    const auto str = to_string_representation(internal_schema);
+    return SchemaHolder<T, Ps...>{Schema<T>::from_string(str)};
+  }
+};
+
+template <class T, class... Ps>
+static const SchemaHolder<T, Ps...> schema_holder =
+    SchemaHolder<T, Ps...>::make();
+
 /// Returns the Cap'n Proto schema for a class.
 template <class T, class... Ps>
-Schema<T> to_schema() noexcept {
-  const auto internal_schema =
-      parsing::schema::make<Reader, Writer, T,
-                            Processors<SnakeCaseToCamelCase, Ps...>>();
-  const auto str = to_string_representation(internal_schema);
-  return Schema<T>::from_string(str).value();
+Schema<T> to_schema() {
+  return schema_holder<T, Ps...>.schema_.value();
 }
 }  // namespace rfl::capnproto
 
