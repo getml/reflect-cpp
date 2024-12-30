8ed58b72b41d50589770498fa4924278bc1bc7b7
Dr. Patrick Urbanke <patrick@getml.com>
2024-12-30 23:45:20 +0100

    Make sure that the first letter of the snake case is lowercase

--- include/rfl/internal/transform_snake_case.hpp
+++ include/rfl/internal/transform_snake_case.hpp
@@ -15,19 +15,36 @@ consteval char to_upper() {
   }
 }
 
+template <char c>
+consteval char to_lower() {
+  if constexpr (c >= 'A' && c <= 'Z') {
+    return c - ('A' - 'a');
+  } else {
+    return c;
+  }
+}
+
 /// Transforms the field name from snake case to camel case.
 template <internal::StringLiteral _name, bool _capitalize, size_t _i = 0,
           char... chars>
 consteval auto transform_snake_case() {
   if constexpr (_i == _name.arr_.size()) {
     return StringLiteral<sizeof...(chars) + 1>(chars...);
+
   } else if constexpr (_name.arr_[_i] == '_') {
     return transform_snake_case<_name, true, _i + 1, chars...>();
+
   } else if constexpr (_name.arr_[_i] == '\0') {
     return transform_snake_case<_name, false, _name.arr_.size(), chars...>();
+
   } else if constexpr (_capitalize) {
     return transform_snake_case<_name, false, _i + 1, chars...,
                                 to_upper<_name.arr_[_i]>()>();
+
+  } else if constexpr (sizeof...(chars) == 0) {
+    return transform_snake_case<_name, false, _i + 1,
+                                to_lower<_name.arr_[_i]>()>();
+
   } else {
     return transform_snake_case<_name, false, _i + 1, chars...,
                                 _name.arr_[_i]>();
