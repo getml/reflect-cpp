5b59e7995547262f29dc66448746f2f16ee0c4ab
redbg <705642344@qq.com>
2025-02-26 15:57:42 +0800

    fix MSVC warning C4819

--- include/rfl/UnderlyingEnums.hpp
+++ include/rfl/UnderlyingEnums.hpp
@@ -3,7 +3,7 @@
 
 namespace rfl {
 
-/// This is a ‘fake’ processor - it doesn't do much by itself, but its
+/// This is a 'fake' processor - it doesn't do much by itself, but its
 /// its inclusion instructs parsers not to convert enum types to strings, but to integers
 struct UnderlyingEnums {
  public:
