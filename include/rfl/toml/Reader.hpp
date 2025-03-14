99d4b9cbee7d2401fd656edae3a00ad746afe446
Dr. Patrick Urbanke <patrick@getml.com>
2025-03-14 07:05:38 +0100

    Use pointers in the Reader

--- include/rfl/toml/Reader.hpp
+++ include/rfl/toml/Reader.hpp
@@ -20,74 +20,73 @@
 namespace rfl::toml {
 
 struct Reader {
-  using InputArrayType = ::toml::array;
-  using InputObjectType = ::toml::table;
-  using InputVarType = ::toml::value;
+  using InputArrayType = *::toml::array;
+  using InputObjectType = *::toml::table;
+  using InputVarType = *::toml::value;
 
   template <class T>
   static constexpr bool has_custom_constructor =
       (requires(InputVarType var) { T::from_toml_obj(var); });
 
   rfl::Result<InputVarType> get_field_from_array(
-      const size_t _idx, const InputArrayType& _arr) const noexcept {
-    if (_idx >= _arr.size()) {
+      const size_t _idx, const InputArrayType _arr) const noexcept {
+    if (_idx >= _arr->size()) {
       return error("Index " + std::to_string(_idx) + " of of bounds.");
     }
-    return _arr[_idx];
+    return (*_arr)[_idx];
   }
 
   rfl::Result<InputVarType> get_field_from_object(
-      const std::string& _name, const InputObjectType& _obj) const noexcept {
-    const auto it = _obj.find(_name);
-    if (it == _obj.end()) {
+      const std::string& _name, const InputObjectType _obj) const noexcept {
+    const auto it = _obj->find(_name);
+    if (it == _obj->end()) {
       return error("Object contains no field named '" + _name + "'.");
     }
     return it->second;
   }
 
-  bool is_empty(const InputVarType& _var) const noexcept {
-    return _var.is_empty();
+  bool is_empty(const InputVarType _var) const noexcept {
+    return _var->is_empty();
   }
 
   template <class T>
-  rfl::Result<T> to_basic_type(const InputVarType& _var) const noexcept {
+  rfl::Result<T> to_basic_type(const InputVarType _var) const noexcept {
     if constexpr (std::is_same<std::remove_cvref_t<T>, std::string>()) {
-      if (!_var.is_string()) {
+      if (!_var->is_string()) {
         return error("Could not cast to std::string!");
       }
-      return _var.as_string();
+      return _var->as_string();
     } else if constexpr (std::is_same<std::remove_cvref_t<T>, bool>()) {
-      if (!_var.is_boolean()) {
+      if (!_var->is_boolean()) {
         return error("Could not cast to bool!");
       }
-      return _var.as_boolean();
+      return _var->as_boolean();
     } else if constexpr (std::is_floating_point<std::remove_cvref_t<T>>()) {
-      if (!_var.is_floating()) {
+      if (!_var->is_floating()) {
         return error("Could not cast to double!");
       }
-      return static_cast<std::remove_cvref_t<T>>(_var.as_floating());
+      return static_cast<std::remove_cvref_t<T>>(_var->as_floating());
     } else if constexpr (std::is_integral<std::remove_cvref_t<T>>()) {
-      if (!_var.is_integer()) {
+      if (!_var->is_integer()) {
         return error("Could not cast to int64_t!");
       }
-      return static_cast<std::remove_cvref_t<T>>(_var.as_integer());
+      return static_cast<std::remove_cvref_t<T>>(_var->as_integer());
     } else {
       static_assert(rfl::always_false_v<T>, "Unsupported type.");
     }
   }
 
-  rfl::Result<InputArrayType> to_array(
-      const InputVarType& _var) const noexcept {
-    if (!_var.is_array()) {
+  rfl::Result<InputArrayType> to_array(const InputVarType _var) const noexcept {
+    if (!_var->is_array()) {
       return error("Could not cast to an array!");
     }
-    return _var.as_array();
+    return &_var->as_array();
   }
 
   template <class ArrayReader>
   std::optional<Error> read_array(const ArrayReader& _array_reader,
-                                  const InputArrayType& _arr) const noexcept {
-    for (const auto& node : _arr) {
+                                  const InputArrayType _arr) const noexcept {
+    for (const auto& node : *_arr) {
       const auto err = _array_reader.read(node);
       if (err) {
         return err;
@@ -99,18 +98,18 @@ struct Reader {
   template <class ObjectReader>
   std::optional<Error> read_object(const ObjectReader& _object_reader,
                                    InputObjectType _obj) const noexcept {
-    for (const auto& [k, v] : _obj) {
+    for (const auto& [k, v] : *_obj) {
       _object_reader.read(std::string_view(k), v);
     }
     return std::nullopt;
   }
 
   rfl::Result<InputObjectType> to_object(
-      const InputVarType& _var) const noexcept {
-    if (!_var.is_table()) {
+      const InputVarType _var) const noexcept {
+    if (!_var->is_table()) {
       return error("Could not cast to a table!");
     }
-    return _var.as_table();
+    return &_var->as_table();
   }
 
   template <class T>
