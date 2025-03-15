30faf2c2851b9991157515a846e917df76a63ec4
Dr. Patrick Urbanke <patrick@getml.com>
2025-03-15 15:54:01 +0100

    Make sure that all textual formats also support std::string_view; #382

--- tests/toml/write_and_read.hpp
+++ tests/toml/write_and_read.hpp
@@ -11,7 +11,8 @@ template <class... Ps>
 void write_and_read(const auto& _struct) {
   using T = std::remove_cvref_t<decltype(_struct)>;
   const auto serialized1 = rfl::toml::write<Ps...>(_struct);
-  const auto res = rfl::toml::read<T, Ps...>(serialized1);
+  const auto res = rfl::toml::read<T, Ps...>(
+      std::string_view(serialized1.c_str(), serialized1.size()));
   EXPECT_TRUE(res && true) << "Test failed on read. Error: "
                            << res.error().what();
   const auto serialized2 = rfl::toml::write<Ps...>(res.value());
