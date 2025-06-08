ec6ab3baf5d42bec0e2f680ddd65116184a8f28f
Dr. Patrick Urbanke (劉自成) <patrick@getml.com>
2025-06-08 21:00:21 +0200

    Improved support for string_views for TOML and XML (#445)
    * Improved support for string_views for TOML and XML
    
    * Upgrade to pugixml 1.15 in order to be able to use string views

--- src/rfl/xml/Writer.cpp
+++ src/rfl/xml/Writer.cpp
@@ -78,8 +78,8 @@ Writer::OutputArrayType Writer::add_array_to_object(
 
 Writer::OutputVarType Writer::add_value_to_array_impl(
     const std::string& _str, OutputArrayType* _parent) const noexcept {
-  auto node_child = Ref<pugi::xml_node>::make(
-      _parent->node_->append_child(_parent->name_.data()));
+  auto node_child =
+      Ref<pugi::xml_node>::make(_parent->node_->append_child(_parent->name_));
   node_child->append_child(pugi::node_pcdata).set_value(_str.c_str());
   return OutputVarType(node_child);
 }
@@ -88,14 +88,14 @@ Writer::OutputVarType Writer::add_value_to_object_impl(
     const std::string_view& _name, const std::string& _str,
     OutputObjectType* _parent, const bool _is_attribute) const noexcept {
   if (_is_attribute) {
-    _parent->node_->append_attribute(_name.data()) = _str.c_str();
+    _parent->node_->append_attribute(_name) = _str.c_str();
     return OutputVarType(_parent->node_);
   } else if (_name == XML_CONTENT) {
     _parent->node_->append_child(pugi::node_pcdata).set_value(_str.c_str());
     return OutputVarType(_parent->node_);
   } else {
     auto node_child =
-        Ref<pugi::xml_node>::make(_parent->node_->append_child(_name.data()));
+        Ref<pugi::xml_node>::make(_parent->node_->append_child(_name));
     node_child->append_child(pugi::node_pcdata).set_value(_str.c_str());
     return OutputVarType(node_child);
   }
@@ -103,8 +103,8 @@ Writer::OutputVarType Writer::add_value_to_object_impl(
 
 Writer::OutputObjectType Writer::add_object_to_array(
     const size_t _size, OutputArrayType* _parent) const noexcept {
-  auto node_child = Ref<pugi::xml_node>::make(
-      _parent->node_->append_child(_parent->name_.data()));
+  auto node_child =
+      Ref<pugi::xml_node>::make(_parent->node_->append_child(_parent->name_));
   return OutputObjectType(node_child);
 }
 
@@ -112,14 +112,14 @@ Writer::OutputObjectType Writer::add_object_to_object(
     const std::string_view& _name, const size_t _size,
     OutputObjectType* _parent) const noexcept {
   auto node_child =
-      Ref<pugi::xml_node>::make(_parent->node_->append_child(_name.data()));
+      Ref<pugi::xml_node>::make(_parent->node_->append_child(_name));
   return OutputObjectType(node_child);
 }
 
 Writer::OutputVarType Writer::add_null_to_array(
     OutputArrayType* _parent) const noexcept {
-  auto node_child = Ref<pugi::xml_node>::make(
-      _parent->node_->append_child(_parent->name_.data()));
+  auto node_child =
+      Ref<pugi::xml_node>::make(_parent->node_->append_child(_parent->name_));
   return OutputVarType(node_child);
 }
 
@@ -132,7 +132,7 @@ Writer::OutputVarType Writer::add_null_to_object(
     return OutputVarType(_parent->node_);
   } else {
     auto node_child =
-        Ref<pugi::xml_node>::make(_parent->node_->append_child(_name.data()));
+        Ref<pugi::xml_node>::make(_parent->node_->append_child(_name));
     return OutputVarType(node_child);
   }
 }
