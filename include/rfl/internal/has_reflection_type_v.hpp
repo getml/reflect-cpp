617ec90b5c61169bc2cb7ff2a296c1d6dc01e79b
Dr. Patrick Urbanke <patrick@getml.com>
2025-08-16 16:53:52 +0200

    Used more idiomatic solutions for has_reflection_method_v and has_reflection_type_v

--- include/rfl/internal/has_reflection_type_v.hpp
+++ include/rfl/internal/has_reflection_type_v.hpp
@@ -1,33 +1,20 @@
 #ifndef RFL_HASREFLECTIONTYPEV_HPP_
 #define RFL_HASREFLECTIONTYPEV_HPP_
 
-#include <cstdint>
-#include <utility>
+#include <concepts>
 
-namespace rfl {
-namespace internal {
+namespace rfl::internal {
 
-template <class Wrapper>
-class HasReflectionType {
-   private:
-    template <class U>
-    static std::int64_t foo(...);
+template <class T>
+struct ReflectionTypeWrapper {};
 
-    template <class U>
-    static std::int32_t foo(typename U::ReflectionType*);
-
-   public:
-    static constexpr bool value =
-        sizeof(foo<Wrapper>(nullptr)) == sizeof(std::int32_t);
+template <typename T>
+constexpr bool has_reflection_type_v = requires() {
+  {
+    ReflectionTypeWrapper<typename T::ReflectionType>{}
+  } -> std::same_as<ReflectionTypeWrapper<typename T::ReflectionType>>;
 };
 
-/// Utility parameter for named tuple parsing, can be used by the
-/// parsers to determine whether a class or struct defines a type
-/// called "ReflectionType".
-template <typename Wrapper>
-constexpr bool has_reflection_type_v = HasReflectionType<Wrapper>::value;
-
-}  // namespace internal
-}  // namespace rfl
+}  // namespace rfl::internal
 
 #endif  // RFL_HASNAMEDTUPLETYPEV_HPP_
