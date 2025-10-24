87b2b18d049c1ccb10bc412036bab6a3595e011e
Dr. Patrick Urbanke <patrick@getml.com>
2025-10-24 23:51:19 +0200

    Recreated the original state of the enchantum thirdparty library

--- include/rfl/thirdparty/enchantum/details/format_util.hpp
+++ include/rfl/thirdparty/enchantum/details/format_util.hpp
@@ -1,24 +1,20 @@
-#pragma once
-
-#include <string>
-
-#include "../bitflags.hpp"
 #include "../enchantum.hpp"
+#include <string>
 
 namespace enchantum {
 namespace details {
-template <Enum E>
-std::string format(E e) noexcept {
-  if constexpr (is_bitflag<E>) {
-    if (const auto name = enchantum::to_string_bitflag(e); !name.empty())
-      return std::string(name.data(), name.size());
-  } else {
-    if (const auto name = enchantum::to_string(e); !name.empty())
-      return std::string(name.data(), name.size());
+  template<Enum E>
+  std::string format(E e) noexcept
+  {
+    if constexpr (is_bitflag<E>) {
+      if (const auto name = enchantum::to_string_bitflag(e); !name.empty())
+        return std::string(name.data(), name.size());
+    }
+    else {
+      if (const auto name = enchantum::to_string(e); !name.empty())
+        return std::string(name.data(), name.size());
+    }
+    return std::to_string(+enchantum::to_underlying(e)); // promote using + to select int overload if to underlying returns char
   }
-  return std::to_string(
-      +enchantum::to_underlying(e));  // promote using + to select int overload
-                                      // if to underlying returns char
-}
-}  // namespace details
-}  // namespace enchantum
+} // namespace details
+} // namespace enchantum
--- include/rfl/thirdparty/enchantum/istream.hpp
+++ include/rfl/thirdparty/enchantum/istream.hpp
@@ -1,27 +1,26 @@
 #pragma once
+#include "enchantum.hpp"
 #include <concepts>
-#include <ios>
 #include <iosfwd>
 #include <string>
 
-#include "bitflags.hpp"
-#include "enchantum.hpp"
-
 namespace enchantum::istream_operators {
-template <typename Traits, Enum E>
+template<typename Traits, Enum E>
   requires std::assignable_from<E&, E>
-std::basic_istream<char, Traits>& operator>>(
-    std::basic_istream<char, Traits>& is, E& value) {
+std::basic_istream<char, Traits>& operator>>(std::basic_istream<char, Traits>& is, E& value)
+{
   std::basic_string<char, Traits> s;
   is >> s;
-  if (!is) return is;
+  if (!is)
+    return is;
 
   if constexpr (is_bitflag<E>) {
     if (const auto v = enchantum::cast_bitflag<E>(s))
       value = *v;
     else
       is.setstate(std::ios_base::failbit);
-  } else {
+  }
+  else {
     if (const auto v = enchantum::cast<E>(s))
       value = *v;
     else
@@ -30,4 +29,4 @@ std::basic_istream<char, Traits>& operator>>(
   return is;
 }
 
-}  // namespace enchantum::istream_operators
+} // namespace enchantum::istream_operators
