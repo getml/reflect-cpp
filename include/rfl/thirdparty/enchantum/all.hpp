c528e26dec0af8e2da267c2e6aeb0d9a6865d7cf
Dr. Patrick Urbanke <patrick@getml.com>
2025-10-24 23:20:09 +0200

    Fixed include

--- include/rfl/thirdparty/enchantum/all.hpp
+++ include/rfl/thirdparty/enchantum/all.hpp
@@ -1,20 +1,14 @@
 #pragma once
-#include "details/optional.hpp"
-#include "details/string_view.hpp"
-#include "details/generate_arrays.hpp"
-
 #include "algorithms.hpp"
 #include "array.hpp"
 #include "bitflags.hpp"
+#include "bitset.hpp"
 #include "bitwise_operators.hpp"
+#include "details/generate_arrays.hpp"
+#include "details/optional.hpp"
+#include "details/string_view.hpp"
 #include "enchantum.hpp"
 #include "entries.hpp"
 #include "iostream.hpp"
 #include "next_value.hpp"
-#include "bitset.hpp"
-
-#if __has_include(<fmt/format.h>)
-  #include "fmt_format.hpp"
-#elif __has_include(<format>)
-  #include "std_format.hpp"
-#endif
+#include "std_format.hpp"
