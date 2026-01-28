c7ccae8cabd508faa3b7c2afc9ed4943a3a06602
makekryl <makekryl@gmail.com>
2026-01-28 10:32:22 +0300

    Fix some unused parameters warnings (#582)

--- include/rfl/parsing/FieldVariantParser.hpp
+++ include/rfl/parsing/FieldVariantParser.hpp
@@ -73,7 +73,7 @@ struct FieldVariantParser {
 
   static schema::Type to_schema(
       std::map<std::string, schema::Type>* _definitions,
-      [[maybe_unused]] std::vector<schema::Type> _types = {}) {
+      std::vector<schema::Type> = {}) {
     using VariantType = rfl::Variant<NamedTuple<FieldTypes>...>;
     return Parser<R, W, VariantType, ProcessorsType>::to_schema(_definitions);
   }
