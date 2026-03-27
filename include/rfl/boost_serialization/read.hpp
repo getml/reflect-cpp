13db6f0a0498fa20003d0f6bece8f84d8cadab8d
Ron0Studios <ron0studios@users.noreply.github.com>
2026-03-27 12:10:27 +0000

    boost serialisation (read/write/load)

---
+++ include/rfl/boost_serialization/read.hpp
@@ -0,0 +1,69 @@
+#ifndef RFL_BOOST_SERIALIZATION_READ_HPP_
+#define RFL_BOOST_SERIALIZATION_READ_HPP_
+
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/serialization/string.hpp>
+
+#include <istream>
+#include <sstream>
+#include <string>
+
+#include "../Processors.hpp"
+#include "../concepts.hpp"
+#include "../internal/wrap_in_rfl_array_t.hpp"
+#include "Parser.hpp"
+#include "Reader.hpp"
+
+namespace rfl::boost_serialization {
+
+namespace detail {
+
+template <class T, class IArchive, class OArchive, class... Ps>
+auto read_from_archive(IArchive& _ar) {
+  using R = Reader<IArchive>;
+  auto r = R();
+  auto var = typename R::InputVarType{&_ar};
+  return Parser<IArchive, OArchive, T, Processors<Ps...>>::read(r, var);
+}
+
+}  // namespace detail
+
+/// Parses an object from bytes using a binary archive.
+template <class T, class... Ps>
+Result<internal::wrap_in_rfl_array_t<T>> read(
+    const concepts::ByteLike auto* _bytes, const size_t _size) {
+  try {
+    std::string str(reinterpret_cast<const char*>(_bytes), _size);
+    std::istringstream stream(str);
+    boost::archive::binary_iarchive ar(stream);
+    return detail::read_from_archive<T, boost::archive::binary_iarchive,
+                                     boost::archive::binary_oarchive,
+                                     Ps...>(ar);
+  } catch (std::exception& e) {
+    return error(e.what());
+  }
+}
+
+/// Parses an object from a byte container using a binary archive.
+template <class T, class... Ps>
+auto read(const concepts::ContiguousByteContainer auto& _bytes) {
+  return read<T, Ps...>(_bytes.data(), _bytes.size());
+}
+
+/// Parses an object from a stream using a binary archive.
+template <class T, class... Ps>
+auto read(std::istream& _stream) {
+  try {
+    boost::archive::binary_iarchive ar(_stream);
+    return detail::read_from_archive<T, boost::archive::binary_iarchive,
+                                     boost::archive::binary_oarchive,
+                                     Ps...>(ar);
+  } catch (std::exception& e) {
+    return Result<internal::wrap_in_rfl_array_t<T>>(error(e.what()));
+  }
+}
+
+}  // namespace rfl::boost_serialization
+
+#endif
