4eb0f9df21f81c0c085cdb87eb2b382d4dff61cd
Dr. Patrick Urbanke (劉自成) <patrick@getml.com>
2026-07-09 23:39:25 +0200

    Added test for nested vars

---
+++ tests/env/test_nested.cpp
@@ -0,0 +1,52 @@
+#include <gtest/gtest.h>
+
+#include <rfl.hpp>
+#include <rfl/env.hpp>
+#include <string>
+
+namespace test_nested {
+
+struct NestedSettings {
+  std::string database_host;
+  int database_port;
+};
+
+struct Settings {
+  std::string host;
+  int port;
+  double rate;
+  bool verbose;
+  NestedSettings nested;
+};
+
+TEST(env, test_nested) {
+  const auto settings = Settings{.host = "localhost",
+                                 .port = 8080,
+                                 .rate = 1.5,
+                                 .verbose = true,
+                                 .nested = NestedSettings{
+                                     .database_host = "localhost",
+                                     .database_port = 8080,
+                                 }};
+
+  rfl::env::write(settings);
+
+  ASSERT_EQ(std::getenv("HOST"), std::string("localhost"));
+  ASSERT_EQ(std::getenv("PORT"), std::string("8080"));
+  ASSERT_EQ(std::getenv("RATE"), std::string("1.500000"));
+  ASSERT_EQ(std::getenv("VERBOSE"), std::string("true"));
+  ASSERT_EQ(std::getenv("NESTED_DATABASE_HOST"), std::string("localhost"));
+  ASSERT_EQ(std::getenv("NESTED_DATABASE_PORT"), std::string("8080"));
+
+  const auto read_settings = rfl::env::read<Settings>();
+
+  ASSERT_TRUE(read_settings);
+  ASSERT_EQ(read_settings->host, "localhost");
+  ASSERT_EQ(read_settings->port, 8080);
+  ASSERT_DOUBLE_EQ(read_settings->rate, 1.5);
+  ASSERT_EQ(read_settings->verbose, true);
+  ASSERT_EQ(read_settings->nested.database_host, "localhost");
+  ASSERT_EQ(read_settings->nested.database_port, 8080);
+}
+
+}  // namespace test_nested
