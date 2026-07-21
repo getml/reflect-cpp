b17d40401e3d1ad174286669a236f841dc1dd878
Dr. Patrick Urbanke <urbanke.patrick@gmail.com>
2026-07-21 20:13:42 +0200

    Support sets with custom compare and allocater; fixes #690

---
+++ tests/json/test_multiset_with_custom_comparator_and_allocator.cpp
@@ -0,0 +1,32 @@
+#include <rfl.hpp>
+#include <rfl/json.hpp>
+#include <set>
+#include <string>
+
+#include "write_and_read.hpp"
+
+namespace test_multiset_with_custom_comparator_and_allocator {
+
+struct CustomLess {
+  bool operator()(const int& a, const int& b) const { return a < b; }
+};
+
+struct Person {
+  rfl::Rename<"firstName", std::string> first_name;
+  rfl::Rename<"lastName", std::string> last_name = "Simpson";
+  std::unique_ptr<std::multiset<int, CustomLess, std::allocator<int>>> ages;
+};
+
+TEST(json, test_multiset_with_custom_comparator_and_allocator) {
+  auto ages =
+      std::make_unique<std::multiset<int, CustomLess, std::allocator<int>>>(
+          std::multiset<int, CustomLess, std::allocator<int>>({20, 30, 30, 40}));
+
+  const auto homer =
+      Person{.first_name = "Homer", .ages = std::move(ages)};
+
+  write_and_read(
+      homer,
+      R"({"firstName":"Homer","lastName":"Simpson","ages":[20,30,30,40]})");
+}
+}  // namespace test_multiset_with_custom_comparator_and_allocator
