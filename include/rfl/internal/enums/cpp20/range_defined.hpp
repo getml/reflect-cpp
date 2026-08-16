cd0961920e4005af39fd1a9f7d9b2db40cba6c9b
Dr. Patrick Urbanke <urbanke.patrick@gmail.com>
2026-08-16 20:50:11 +0200

    Updated range_defined

--- include/rfl/internal/enums/cpp20/range_defined.hpp
+++ include/rfl/internal/enums/cpp20/range_defined.hpp
@@ -1,9 +1,9 @@
-#ifndef RFL_INTERNAL_ENUMS_RANGE_DEFINED_HPP_
-#define RFL_INTERNAL_ENUMS_RANGE_DEFINED_HPP_
+#ifndef RFL_INTERNAL_ENUMS_CPP20_RANGE_DEFINED_HPP_
+#define RFL_INTERNAL_ENUMS_CPP20_RANGE_DEFINED_HPP_
 
 #include "../../../config.hpp"
 
-namespace rfl::internal::enums {
+namespace rfl::internal::enums::cpp20 {
 
 template <class E>
 concept range_defined = requires {
@@ -12,6 +12,6 @@ concept range_defined = requires {
   { config::enum_range<E>::max };
 };
 
-}  // namespace rfl::internal::enums
+}  // namespace rfl::internal::enums::cpp20
 
 #endif
