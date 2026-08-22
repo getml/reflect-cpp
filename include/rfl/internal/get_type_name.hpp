ff054448ac9436b50e9fc380611f6ff4054dfa4f
Dr. Patrick Urbanke <urbanke.patrick@gmail.com>
2026-08-22 18:32:13 +0200

    Get type name using C++ 26

--- include/rfl/internal/get_type_name.hpp
+++ include/rfl/internal/get_type_name.hpp
@@ -1,21 +1,21 @@
 #ifndef RFL_INTERNAL_GETTYPENAME_HPP_
 #define RFL_INTERNAL_GETTYPENAME_HPP_
-// TODO
-// #ifndef REFLECTCPP_USE_CPP26_REFLECTION
+
+#ifndef REFLECTCPP_USE_CPP26_REFLECTION
 #include "cpp20/get_type_name.hpp"
-// #else
-//  TODO
-// #endif
+#else
+#include "cpp26/get_type_name.hpp"
+#endif
 
 namespace rfl::internal {
 
 template <class T>
 consteval auto get_type_name() {
-  // #ifndef REFLECTCPP_USE_CPP26_REFLECTION
+#ifndef REFLECTCPP_USE_CPP26_REFLECTION
   return cpp20::get_type_name<T>();
-  // #else
-  //  TODO
-  // #endif
+#else
+  return cpp26::get_type_name<T>();
+#endif
 }
 
 }  // namespace rfl::internal
